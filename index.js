06e1259b62a5c7e7f98f44e1728aef2dd3600f0a
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-21 14:46:39 +0100

    create basic calc functions

---
+++ index.js
@@ -0,0 +1,22 @@
+///basic calc functions
+
+function Add(a, b) {
+  return a + b;
+}
+
+function Subtract(a, b) {
+  return a - b;
+}
+
+function Multiply(a, b) {
+  return a * b;
+}
+
+function Divide(a, b) {
+  return a / b;
+}
+
+console.log(Add(3, 4));
+console.log(Subtract(10, 4));
+console.log(Multiply(3, 4));
+console.log(Divide(10, 2));
