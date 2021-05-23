25bb4495548f60e16a8e7015ed6ed82c37e69a17
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-23 12:02:35 +0100

    query select all btns

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ function Operate(action, a, b) {
   return action(a, b);
 }
 
-console.log(Operate(Add, 4, 1));
-console.log(Operate(Divide, 4, 2));
-console.log(Operate(Multiply, 4, 3));
-console.log(Operate(Subtract, 4, 1));
+// console.log(Operate(Add, 4, 1));
+// console.log(Operate(Divide, 4, 2));
+// console.log(Operate(Multiply, 4, 3));
+// console.log(Operate(Subtract, 4, 1));
+
+const nums = document.querySelectorAll("num");
+const operator = document.querySelectorAll("operator");
+const clear = document.querySelector("clear");
+const del = document.querySelector("del");
+const equal = document.querySelector("equal");
+const point = document.querySelector("decimal");
