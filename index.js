c184163500994a74961eedca35790fe6b9e3a7ff
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-23 12:18:34 +0100

    add clear display function

--- index.js
+++ index.js
@@ -16,25 +16,29 @@ function Divide(a, b) {
   return a / b;
 }
 
-// console.log(Add(3, 4));
-// console.log(Subtract(10, 4));
-// console.log(Multiply(3, 4));
-// console.log(Divide(10, 2));
-
 function Operate(action, a, b) {
   return action(a, b);
 }
 
-// console.log(Operate(Add, 4, 1));
-// console.log(Operate(Divide, 4, 2));
-// console.log(Operate(Multiply, 4, 3));
-// console.log(Operate(Subtract, 4, 1));
+const nums = document.querySelectorAll(".num");
+const operator = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const del = document.querySelector(".del");
+const equal = document.querySelector(".equal");
+const point = document.querySelector(".decimal");
 
-const nums = document.querySelectorAll("num");
-const operator = document.querySelectorAll("operator");
-const clear = document.querySelector("clear");
-const del = document.querySelector("del");
-const equal = document.querySelector("equal");
-const point = document.querySelector("decimal");
+const display = document.querySelector(".screen");
+// console.log(display.innerHTML);
 
-const display = document.querySelector("screen");
+nums.forEach((num) => num.addEventListener("click", DisplayValue));
+
+clear.addEventListener("click", Clear);
+//functions
+
+function Clear() {
+  display.innerText = "";
+}
+
+function DisplayValue(num) {
+  display.innerText += num.innerText;
+}
